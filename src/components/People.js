bf7d7fc56c7a9a041c83e2f124590b980e701a07
Ahmed Lemeš <85510727+hmoa01@users.noreply.github.com>
2022-09-05 11:31:39 +0200

    some css update

---
+++ src/components/People.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./People.css"
+
+function People() {
+    const [people, setPeople] = useState([]);
+    useEffect(()=>{
+        axios.get("https://api.tvmaze.com/people")
+        .then((response)=>{
+            console.log(response.data);
+            setPeople(response.data)
+        })
+    },[]);
+
+    return (
+        <div className="gallery">
+            {people.map(p => {
+                return (
+                    <div className="single-img">
+                        {p.image && <img src={p.image.medium} />}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default People;
